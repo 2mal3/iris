b7faf329541bf08df0ee10fddc752c0fc5807a99
2mal3 <56305732+2mal3@users.noreply.github.com>
2024-04-05 17:13:52 +0200

    fix: file read pointer sometimes won't reset

--- utils.go
+++ utils.go
@@ -25,17 +25,21 @@ func doesFileExist(path string) bool {
 }
 
 func getFileHash(file *os.File) (string, error) {
+	// Reset the file's read position because we want to use the same file object again
+	defer file.Seek(0, 0)
+
 	hash := sha256.New()
 	if _, err := io.Copy(hash, file); err != nil {
 		return "", err
 	}
-	// Reset the file's read position because we want to use the same file object again
-	defer file.Seek(0, 0)
 
 	return fmt.Sprintf("%x", hash.Sum(nil)), nil
 }
 
 func copyFile(sourceFile *os.File, destPath string) error {
+	// Reset the file's read position because we want to use the same file object again
+	defer sourceFile.Seek(0, 0)
+
 	desFile, err := os.Create(destPath)
 	if err != nil {
 		return err
@@ -46,21 +50,20 @@ func copyFile(sourceFile *os.File, destPath string) error {
 	if err != nil {
 		return err
 	}
-	// Reset the file's read position because we want to use the same file object again
-	defer sourceFile.Seek(0, 0)
 
 	return nil
 }
 
 func getFileContentType(file *os.File) (string, error) {
+	// Reset the file's read position because we want to use the same file object again
+	defer file.Seek(0, 0)
+
 	// Read the first 512 bytes of the file
 	buffer := make([]byte, 512)
 	_, err := file.Read(buffer)
 	if err != nil {
 		return "", err
 	}
-	// Reset the file's read position because we want to use the same file object again
-	defer file.Seek(0, 0)
 
 	// Detect the content type based on the file header
 	contentType := http.DetectContentType(buffer)
@@ -68,12 +71,13 @@ func getFileContentType(file *os.File) (string, error) {
 }
 
 func getImageCreationTime(file *os.File) (time.Time, error) {
+	// Reset the file's read position because we want to use the same file object again
+	defer file.Seek(0, 0)
+
 	data, err := exif.Decode(file)
 	if err != nil {
 		return time.Time{}, err
 	}
-	// Reset the file's read position because we want to use the same file object again
-	defer file.Seek(0, 0)
 
 	creationTime, err := data.DateTime()
 	if err != nil {
@@ -84,6 +88,9 @@ func getImageCreationTime(file *os.File) (time.Time, error) {
 }
 
 func getVideoCreationTime(file *os.File) (time.Time, error) {
+	// Reset the file's read position because we want to use the same file object again
+	defer file.Seek(0, 0)
+
 	ctx, cancelFn := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancelFn()
 
@@ -91,8 +98,6 @@ func getVideoCreationTime(file *os.File) (time.Time, error) {
 	if err != nil {
 		return time.Time{}, err
 	}
-	// Reset the file's read position because we want to use the same file object again
-	defer file.Seek(0, 0)
 
 	creationTimeString, err := data.Format.TagList.GetString("creation_time")
 	if err != nil {
