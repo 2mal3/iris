8972c1b5c0271d5c6abd2591a8d2a930791ff665
2mal3 <56305732+2mal3@users.noreply.github.com>
2024-08-10 01:33:18 +0200

    chore: move to typical command line
    no yaml files anymore
    use std input, output and error

---
+++ cmd/root.go
@@ -0,0 +1,56 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/2mal3/iris/pkg"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:           "iris",
+	Short:         "Moin",
+	Long:          "",
+	RunE:          run,
+	SilenceErrors: true,
+}
+
+var inputPaths []string
+var outputPath string
+var moveFiles bool
+var removeDuplicates bool
+
+func init() {
+	rootCmd.Flags().StringSliceVarP(&inputPaths, "inputPaths", "i", []string{}, "Input paths")
+	rootCmd.MarkFlagRequired("inputPaths")
+
+	rootCmd.Flags().StringVarP(&outputPath, "outputPath", "o", "", "Output path")
+	rootCmd.MarkFlagRequired("outputPath")
+
+	rootCmd.Flags().BoolVarP(&moveFiles, "moveFiles", "m", false, "Move files")
+
+	rootCmd.Flags().BoolVarP(&removeDuplicates, "removeDuplicates", "r", false, "Remove duplicate files")
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err.Error())
+		os.Exit(1)
+	}
+	rootCmd.Execute()
+}
+
+func run(cmd *cobra.Command, args []string) error {
+	config := pkg.Config{
+		InputPaths:       inputPaths,
+		OutputPath:       outputPath,
+		MoveFiles:        moveFiles,
+		RemoveDuplicates: removeDuplicates,
+	}
+
+	if err := pkg.Main(config); err != nil {
+		return err
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -6,5 +6,10 @@ require (
 	github.com/rwcarlsen/goexif v0.0.0-20190401172101-9e8deecbddbd
 	github.com/wneessen/go-fileperm v0.2.1
 	gopkg.in/vansante/go-ffprobe.v2 v2.1.1
-	gopkg.in/yaml.v3 v3.0.1
+	github.com/spf13/cobra v1.8.1
+)
+
+require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 )
